3914555372270a13daaf1ca3c7bd5e5a540535b3
Tom Price <tomgprice90@gmail.com>
2023-04-18 06:48:16 +0100

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p></p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Fib Calculator
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
